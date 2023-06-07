f8cb913730a5bbb82e7c53e9be0b72beff8f9f06
AstridABG <l.n.andreagarcia@hotmail.com>
2023-06-07 02:00:01 -0600

    cambioshtmlJSyCSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import analyzer from './analyzer.js';
 
+
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+var area = document.querySelector('textarea');
+
+area.addEventListener('input', function() {
+  // se escucha cada evento de teclado en el textarea con el name 'user-input'
+}
