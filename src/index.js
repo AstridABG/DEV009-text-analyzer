390fb6ce8eb42be3ffade4d42985614daccef348
AstridABG <l.n.andreagarcia@hotmail.com>
2023-06-07 02:07:35 -0600

    actualizacion variable

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
-var area = document.querySelector('textarea');
+let area = document.querySelector('textarea');
 
 area.addEventListener('input', function() {
   // se escucha cada evento de teclado en el textarea con el name 'user-input'
