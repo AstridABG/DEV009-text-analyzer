8f59b697243c71e5fcf887da8948e5622971faaa
AstridABG <l.n.andreagarcia@hotmail.com>
2023-06-09 00:00:03 -0600

    primera funcion de JS que escucha los eventos del textarea

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import analyzer from './analyzer.js';
 
-
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
-
 let area = document.querySelector('textarea');
-
 area.addEventListener('input', function() {
   // se escucha cada evento de teclado en el textarea con el name 'user-input'
-}
+
+  let li =  document.querySelector('.metricas').getElementsByTagName('li');
+  //document.querySelector("ul");
+ li[0].innerHTML=area.value;
+ li[1].innerHTML=area.value;
+ li[2].innerHTML=area.value;
+ li[3].innerHTML=area.value;
+ li[4].innerHTML=area.value;
+ li[5].innerHTML=area.value;
+
+ //alert("el valor del li en el indice 0 es "+li[0].innerHTML);
+ // alert("el valor del li en el indice 1 es "+li[1].innerHTML);
+//  alert("el valor del li en el indice 2 es "+li[2].innerHTML);
+//alert("el valor del li en el indice 3 es "+li[3].innerHTML);
+//    alert("el valor del li en el indice 4 es "+li[4].innerHTML);
+//  alert("el valor del li en el indice 5 es "+li[5].innerHTML);
+ /* for(let indice=0; indice<li.length; indice++){
+      alert("estoy en el indice: "+indice);
+      alert("el valor del li en el indice "+indice+" es "+li[indice].innerHTML);
+//        li[indice].innerHTML = "hola"+iindice;
+
+  }*/
+}, false);
