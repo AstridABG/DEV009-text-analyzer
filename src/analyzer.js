54fe51a7f45251ccb78c31ff6d23eacf63c20d3d
AstridABG <l.n.andreagarcia@hotmail.com>
2023-06-13 00:15:29 -0600

    Funciones analyzer revisar tests

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,101 +1,135 @@
 const analyzer = {  
-  getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  let textWOSpaceStart = text.value.trimStart();
-  let arrayOfText = textWOSpaceStart.split(" "); 
-   //alert(arrayOfText.length);
-  return arrayOfText.length;
-  },
-
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-    let textWOSpaceStart = text.value.trimStart();
+    const textWOSpaceStart = text.value; //.trimStart();
     return textWOSpaceStart.length;
   },
-
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   
-  let strWODots = text.value.replaceAll(".", "");
-  let strWOComma = strWODots.replaceAll(",", "");
-  let strWOColon = strWOComma.replaceAll(":", "");
-  let strWOSemicolon = strWOColon.replaceAll(";", "");
-  let strWODash = strWOSemicolon.replaceAll("-", "");
-  let strWOApostrophe = strWODash.replaceAll("\'", "");
-  let strWOQuest1 = strWOApostrophe.replaceAll("¿", "");
-  let strWOQuest2 = strWOQuest1.replaceAll("?", "");
-  let strWOExclam1 = strWOQuest2.replaceAll("¡", "");
-  let strWOExclam2 = strWOExclam1.replaceAll("!", "");
-  let strWOBracket1 = strWOExclam2.replaceAll("[", "");
-  let strWOBracket2 = strWOBracket1.replaceAll("]", "");
-  let strWOParent1 = strWOBracket2.replaceAll("(", "");
-  let strWOParent2 = strWOParent1.replaceAll(")", "");
-  let strWOAste = strWOParent2.replaceAll("*", "");
-  let strWOQuote = strWOAste.replaceAll("\"", "");
-  let strWOUnderScore = strWOQuote.replaceAll("_", "");
-  let strWOEspace = strWOUnderScore.replaceAll(" ", "");
+    const strWODots = text.value.replaceAll(".", "");
+    const strWOComma = strWODots.replaceAll(",", "");
+    const strWOColon = strWOComma.replaceAll(":", "");
+    const strWOSemicolon = strWOColon.replaceAll(";", "");
+    const strWODash = strWOSemicolon.replaceAll("-", "");
+    const strWOApostrophe = strWODash.replaceAll("'", "");
+    const strWOQuest1 = strWOApostrophe.replaceAll("¿", "");
+    const strWOQuest2 = strWOQuest1.replaceAll("?", "");
+    const strWOExclam1 = strWOQuest2.replaceAll("¡", "");
+    const strWOExclam2 = strWOExclam1.replaceAll("!", "");
+    const strWOBracket1 = strWOExclam2.replaceAll("[", "");
+    const strWOBracket2 = strWOBracket1.replaceAll("]", "");
+    const strWOParent1 = strWOBracket2.replaceAll("(", "");
+    const strWOParent2 = strWOParent1.replaceAll(")", "");
+    const strWOAste = strWOParent2.replaceAll("*", "");
+    const strWOQuote = strWOAste.replaceAll("\"", "");
+    const strWOUnderScore = strWOQuote.replaceAll("_", "");
+    const strWOEspace = strWOUnderScore.replaceAll(" ", "");
 
-  return strWOEspace.length;
+    return strWOEspace.length;
 
   },
-
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  
-    let textWOSpaceStart = text.value.trimStart();
-    let arrayOfWords = textWOSpaceStart.split(" ");
-    let characterSum = 0;
-    for (let index=0; index<arrayOfWords.length; index++){
-    
-      characterSum = characterSum + arrayOfWords[index].length;
-      
-    }
-  return characterSum / arrayOfWords.length;
+  getWordCount: (text) => {
+    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const textWOSpaceStart = text.value.trimEnd();
+    const arrayOfText = textWOSpaceStart.split(" "); 
+    //alert(arrayOfText.length);
+    return arrayOfText.length;
+   
   },
 
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    let textWOSpaceStart = text.value.trimStart();
-    let arrayOfZero = textWOSpaceStart.split("0");
-    let arrayOfOne = textWOSpaceStart.split("1");
-    let arrayOfTwo = textWOSpaceStart.split("2");
-    let arrayOfThree = textWOSpaceStart.split("3");
-    let arrayOfFour = textWOSpaceStart.split("4");
-    let arrayOfFive = textWOSpaceStart.split("5");
-    let arrayOfSix = textWOSpaceStart.split("6");
-    let arrayOfSeven = textWOSpaceStart.split("7");
-    let arrayOfEight = textWOSpaceStart.split("8");
-    let arrayOfNine = textWOSpaceStart.split("9");
-    let countOfNumbers = arrayOfZero.length + arrayOfOne.length +
-     arrayOfTwo.length + arrayOfThree.length + arrayOfFour.length +
-      arrayOfFive.length + arrayOfSix.length + arrayOfSeven.length +
-       arrayOfEight.length + arrayOfNine.length; 
-   //alert(countOfNumbers);
-       return countOfNumbers - 10;
+    const textWOSpaceStart = text.value.trimEnd();
+    const arrayOfWords = textWOSpaceStart.split(" ");
+    let characterSum = 0;
+    let counter = 0;
+    for (let index=0; index<arrayOfWords.length; index++){
+     let word = arrayOfWords[index];
+     //console.log(word);
+     //console.log(isNaN(word));
+     if (!isNaN(word)){
+      counter++;
+     }
+      
+    }
+    return counter;
+  
+    /*const textWOSpaceStart = text.value;
+    const arrayOfZero = textWOSpaceStart.split("0");
+    const arrayOfOne = textWOSpaceStart.split("1");
+    const arrayOfTwo = textWOSpaceStart.split("2");
+    const arrayOfThree = textWOSpaceStart.split("3");
+    const arrayOfFour = textWOSpaceStart.split("4");
+    const arrayOfFive = textWOSpaceStart.split("5");
+    const arrayOfSix = textWOSpaceStart.split("6");
+    const arrayOfSeven = textWOSpaceStart.split("7");
+    const arrayOfEight = textWOSpaceStart.split("8");
+    const arrayOfNine = textWOSpaceStart.split("9");
+    const countOfNumbers = arrayOfZero.length + arrayOfOne.length +
+    arrayOfTwo.length + arrayOfThree.length + arrayOfFour.length +
+    arrayOfFive.length + arrayOfSix.length + arrayOfSeven.length +
+    arrayOfEight.length + arrayOfNine.length; 
+    //alert(countOfNumbers);
+    return countOfNumbers - 10; */
 
   },
 
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    let textWOSpaceStart = text.value.trimStart();
-    let arrayOfZero = textWOSpaceStart.split("0");
-    let arrayOfOne = textWOSpaceStart.split("1");
-    let arrayOfTwo = textWOSpaceStart.split("2");
-    let arrayOfThree = textWOSpaceStart.split("3");
-    let arrayOfFour = textWOSpaceStart.split("4");
-    let arrayOfFive = textWOSpaceStart.split("5");
-    let arrayOfSix = textWOSpaceStart.split("6");
-    let arrayOfSeven = textWOSpaceStart.split("7");
-    let arrayOfEight = textWOSpaceStart.split("8");
-    let arrayOfNine = textWOSpaceStart.split("9");
-    let sumOfNumbers = (arrayOfZero.length-1)*0 + (arrayOfOne.length-1)*1 +
+    
+    const textWOSpaceStart = text.value.trimEnd();
+    const arrayOfWords = textWOSpaceStart.split(" ");
+    let numberSum = 0;
+    
+    for (let index=0; index<arrayOfWords.length; index++){
+     let word = arrayOfWords[index];
+     //console.log(word);
+     //console.log(isNaN(word));
+     if (!isNaN(word)){
+      numberSum = numberSum + parseFloat(word);
+     }
+      
+    }
+    return numberSum;
+    /*const textWOSpaceStart = text.value;
+    const arrayOfZero = textWOSpaceStart.split("0");
+    const arrayOfOne = textWOSpaceStart.split("1");
+    const arrayOfTwo = textWOSpaceStart.split("2");
+    const arrayOfThree = textWOSpaceStart.split("3");
+    const arrayOfFour = textWOSpaceStart.split("4");
+    const arrayOfFive = textWOSpaceStart.split("5");
+    const arrayOfSix = textWOSpaceStart.split("6");
+    const arrayOfSeven = textWOSpaceStart.split("7");
+    const arrayOfEight = textWOSpaceStart.split("8");
+    const arrayOfNine = textWOSpaceStart.split("9");
+    const sumOfNumbers = (arrayOfZero.length-1)*0 + (arrayOfOne.length-1)*1 +
     (arrayOfTwo.length-1)*2 + (arrayOfThree.length-1)*3 + (arrayOfFour.length-1)*4 +
     (arrayOfFive.length-1)*5 + (arrayOfSix.length-1)*6 + (arrayOfSeven.length-1)*7 +
     (arrayOfEight.length-1)*8 + (arrayOfNine.length-1)*9; 
-   //alert(countOfNumbers);
-       return sumOfNumbers ;
+    //alert(countOfNumbers);
+    return sumOfNumbers ;*/
  
   },
+
+  getAverageWordLength: (text) => {    
+    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  
+    const textWOSpaceStart = text.value;
+    const arrayOfWords = textWOSpaceStart.split(" ");
+    let characterSum = 0;
+    let division = 0;
+    for (let index=0; index<arrayOfWords.length; index++){
+    
+      characterSum = characterSum + arrayOfWords[index].length;
+      division =  characterSum / arrayOfWords.length;
+    }
+    return division.toFixed(3);
+  },
+
+
+
+
 
   
 };
--- src/index.js
+++ src/index.js
@@ -2,29 +2,28 @@ import analyzer from './analyzer.js';
 
 // analyzer.getAlert("Hola analyzer");
 
-let area = document.querySelector('textarea');
+const area = document.querySelector('textarea');
 area.addEventListener('input', function() {
   // se escucha cada evento de teclado en el textarea con el name 'user-input'
 
-  let li =  document.querySelector('.metricas').getElementsByTagName('li');
+  const li =  document.querySelector('.metricas').getElementsByTagName('li');
   //document.querySelector("ul");
- li[0].innerHTML=analyzer.getWordCount(area);
- li[1].innerHTML=analyzer.getCharacterCount(area);
- li[2].innerHTML=analyzer.getCharacterCountExcludingSpaces(area);
- li[3].innerHTML=analyzer.getAverageWordLength(area);
- li[4].innerHTML=analyzer.getNumberCount(area);
- li[5].innerHTML=analyzer.getNumberSum(area);
-
- //alert("el valor del li en el indice 0 es "+li[0].innerHTML);
- // alert("el valor del li en el indice 1 es "+li[1].innerHTML);
-//  alert("el valor del li en el indice 2 es "+li[2].innerHTML);
-//alert("el valor del li en el indice 3 es "+li[3].innerHTML);
-//    alert("el valor del li en el indice 4 es "+li[4].innerHTML);
-//  alert("el valor del li en el indice 5 es "+li[5].innerHTML);
- /* for(let indice=0; indice<li.length; indice++){
+  li[0].innerHTML=analyzer.getCharacterCount(area);
+  li[1].innerHTML=analyzer.getCharacterCountExcludingSpaces(area);
+  li[2].innerHTML=analyzer.getWordCount(area);
+  li[3].innerHTML=analyzer.getNumberCount(area);
+  li[4].innerHTML=analyzer.getNumberSum(area);
+  li[5].innerHTML=analyzer.getAverageWordLength(area);
+  //alert("el valor del li en el indice 0 es "+li[0].innerHTML);
+  // alert("el valor del li en el indice 1 es "+li[1].innerHTML);
+  //  alert("el valor del li en el indice 2 es "+li[2].innerHTML);
+  //alert("el valor del li en el indice 3 es "+li[3].innerHTML);
+  //    alert("el valor del li en el indice 4 es "+li[4].innerHTML);
+  //  alert("el valor del li en el indice 5 es "+li[5].innerHTML);
+  /* for(let indice=0; indice<li.length; indice++){
       alert("estoy en el indice: "+indice);
       alert("el valor del li en el indice "+indice+" es "+li[indice].innerHTML);
-//        li[indice].innerHTML = "hola"+iindice;
+  //        li[indice].innerHTML = "hola"+iindice;
 
   }*/
 }, false);
